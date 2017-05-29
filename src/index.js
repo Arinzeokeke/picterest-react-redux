6131461c855853d62e7776b6044a09618bc8ae78
Okeke Arinze <trent9002@gmail.com>
2017-05-29 01:46:45 +0100

    add store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
-
-import store 
 import { Provider } from 'react-redux'
+
+import store from './store'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 //import 'bootstrap'
 import { Button } from 'react-bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((<Provider store = {store}>
+	<App />
+	</Provider)
+	, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { promiseMiddleware, localStorageMiddleware } from './middleware';
+import reducer from './reducer';
+
+const getMiddleware = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return applyMiddleware(promiseMiddleware, localStorageMiddleware);
+  } else {
+    // Enable additional logging in non-production environments.
+    return applyMiddleware(promiseMiddleware, localStorageMiddleware, createLogger())
+  }
+}
+
+const store = createStore(reducer, composeWithDevTools(getMiddleware()))
+
+export default store;
