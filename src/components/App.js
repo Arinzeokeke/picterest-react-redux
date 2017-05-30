a3d2d4915ad6bdf26a255c7277aba8e35279835f
Okeke Arinze <trent9002@gmail.com>
2017-05-30 01:48:20 +0100

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import logo from '../logo.svg';
 import '../res/css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = 'container-fluid app'>
+      <Router>
+        <NavContainer />
+        <Switch>
+          <Route exact path = "/" component = {PostsContainer} />
+          <Route path = "/feed" component = {PostsContainer} />
+          <Route path = "/post/:slug" component = {PostContainer} />
+          <Route path = "/@:username" component = {ProfilesContainer} />
+          <Route render = {<div> Nothing </div>} />
+        </Switch>
+      </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import store from './store'
