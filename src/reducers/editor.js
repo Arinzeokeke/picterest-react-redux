ae4b62a23a08d0e27df1b9b63a5ca81655860985
Okeke Arinze <trent9002@gmail.com>
2017-06-28 12:41:34 +0100

    editor reducer add

--- src/reducers/editor.js
+++ src/reducers/editor.js
@@ -3,10 +3,63 @@ import { Types } from '../actions/actions';
 const { 
   EDITOR_PAGE_LOADED,
   EDITOR_PAGE_UNLOADED,
-  ARTICLE_SUBMITTED,
+  POST_SUBMITTED,
   ASYNC_START,
   ADD_TAG,
   REMOVE_TAG,
   UPDATE_FIELD_EDITOR
   } 
   = Types; 
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case EDITOR_PAGE_LOADED:
+      return{
+        ...state,
+        postSlug: action.payload ? action.payload.post.slug : '',
+        title: action.payload ? action.payload.post.title : '',
+        url: action.payload ? action.payload.post.url : '',
+        tagInput: '',
+        tags: action.payload ? action.payload.post.tags : []
+      }
+
+    case POST_SUBMITTED:
+      return {
+        ...state,
+        inProgress: null,
+        errors: action.error ? action.payload.errors : null
+      };
+
+    case ASYNC_START:
+      if (action.subtype === POST_SUBMITTED) {
+        return{
+          ...state,
+          inProgress: true
+        };
+      }
+    case ADD_TAG:
+      return {
+        ...state,
+        tags: state.tags.concat([state.tagInput]),
+        tagInput: ''
+      }
+
+    case REMOVE_TAG:
+    return {
+      ...state,
+      tags: state.tags.filter(tag => tag != action.tag)
+    };
+
+    case UPDATE_FIELD_EDITOR:
+    return {
+      ...state, [action.key]: action.value
+    };
+
+    case EDITOR_PAGE_UNLOADED:
+      return {};
+
+    default:
+      return state;
+  }
+
+};
